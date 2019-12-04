489b1e6590055524837ca774b1cb1b0a96b27b92
Zolboo <zolboo.deb@gmail.com>
2019-12-04 22:19:41 +0100

    DEBUG: font2 init

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "mono:pixelsize=16:antialias=true:autohint=true";
-static char *font2[] = "DejaVu Sans Mono for Powerline:pixelsize=16:antialias=true:autohint=true";
+static char *font2[] = {"DejaVu Sans Mono for Powerline:pixelsize=16:antialias=true:autohint=true";}
 static int borderpx = 2;
 
 /*
